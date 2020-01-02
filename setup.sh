a8acd64658cf67767570d603a4410b39f908e811
Antoine Orsoni <ant.orsoni@gmail.com>
2020-01-02 17:56:19 +0100

    adding a setup.sh.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ python -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
 mv variables_template.py variables.py
-echo "You now have to sart ngrok on port 8080. Then, use ./webhook_tools/post_webhook.py to POST the two webhooks.
-One for when the bot is mentioned, one for when the bot is added in a room"
+printf "You now have to sart ngrok on port 8080. Then, use ./webhook_tools/post_webhook.py to POST the two webhooks.
+* One for when the bot is mentioned,
+* one for when the bot is added in a room.\n
+Don't forget to edit the ./core/variables.py file with your both token and the ngrok URL."
