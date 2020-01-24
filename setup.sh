d047fdf6489226654d47f21c6069926d2d79ced2
AntoineOrsoni <ant.orsoni@gmail.com>
2020-01-24 10:54:11 +0100

    Update README.md

--- setup.sh
+++
@@ -1,8 +0,0 @@
-python3.7 -m venv venv
-source ./venv/bin/activate
-pip install -r requirements.txt
-mv ./core/variables_template.py ./core/variables.py
-printf "You now have to sart ngrok on port 8080. Then, use ./webhook_tools/post_webhook.py to POST the two webhooks.
-* One for when the bot is mentioned,
-* one for when the bot is added in a room.\n
-Don't forget to edit the ./core/variables.py file with your both token and the ngrok URL."
